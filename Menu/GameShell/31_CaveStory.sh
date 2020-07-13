fd63173afde9584b040c30d83dc837ee0a14106a
hi80482 <66435094+hi80482@users.noreply.github.com>
2020-07-13 16:23:22 +0800

    update CaveStory.sh

--- Menu/GameShell/31_CaveStory.sh
+++ Menu/GameShell/31_CaveStory.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-#retroarch -L /home/cpi/.config/retroarch/cores/nxengine_libretro.so /home/cpi/games/nxengine/cavestory/data
 
-retroarch -L /home/cpi/apps/emulators/nxengine_libretro.so /home/cpi/games/nxengine/cavestory/data
+if [ -f "/home/cpi/.config/retroarch/cores/nxengine_libretro.so" ]; then
+    retroarch -L /home/cpi/.config/retroarch/cores/nxengine_libretro.so /home/cpi/games/nxengine/cavestory/data
+else
+    retroarch -L /home/cpi/apps/emulators/nxengine_libretro.so /home/cpi/games/nxengine/cavestory/data
+fi
