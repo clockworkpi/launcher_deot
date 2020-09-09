bb15dce4781511d58fff509b30c67503edaeb796
hi80482 <66435094+hi80482@users.noreply.github.com>
2020-09-09 16:46:50 +0800

    set system volume 50%

--- load.sh
+++ load.sh
@@ -5,6 +5,9 @@ BASENAME=`dirname "$0"`
 cd $BASENAME/sys.py
 
 # boot sound
-mplayer -volume 50 /home/cpi/music/startup.mp3
+mplayer -volume 75 /home/cpi/music/startup.mp3
+
+# set system volume
+amixer sset 'Master' 50%
 
 python run.py
